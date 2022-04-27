b008a8d8ec4ae34b9ce35597f91f73053d2fe34e
Judah George <georgejudah5@gmail.com>
2022-04-27 18:18:26 +0530

    protecting the routes using wrapper function and auth using session instead of tokens

--- flask_todo_list/app.py
+++ flask_todo_list/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
+import functools
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
@@ -13,12 +14,24 @@ class Todo(db.Model):
 
 db.create_all()
 
+def login_required(func):
+    @functools.wraps(func)
+    def secure_function(*args, **kwargs):
+        if "username" not in session:
+            return redirect(url_for("login"))
+        return func()
+    return secure_function
+
 @app.get("/")
+@login_required
 def home():
     todo_list = db.session.query(Todo).all()
-    return render_template('base.html', todo_list=todo_list)
+    if "username" not in session:
+        return redirect(url_for("login"))
+    return render_template('base.html', todo_list=todo_list, name = session['username'])
 
 @app.post("/add")
+@login_required
 def add():
     title = request.form.get("title")
     new_todo = Todo(title=title, complete=False)
@@ -27,6 +40,7 @@ def add():
     return redirect(url_for("home"))
 
 @app.route("/update/<int:todo_id>", methods=["GET", "PUT"])
+@login_required
 def update(todo_id):
     todo = db.session.query(Todo).filter(Todo.id == todo_id).first()
     todo.complete = not todo.complete
@@ -34,12 +48,44 @@ def update(todo_id):
     return redirect(url_for("home"))
 
 @app.route("/delete/<int:todo_id>", methods=["GET","DELETE"])
+@login_required
 def delete(todo_id):
     todo = db.session.query(Todo).filter(Todo.id == todo_id).first()
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("home"))
 
+
+
+#users handling
+users = {"jose": ("jose", "1234")}
+app.secret_key = "jose"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username in users and users[username][1] == password:
+            session["username"] = username
+            return redirect(url_for("home"))
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username not in users:
+            users[username] = (username, password)
+    return render_template("register.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
